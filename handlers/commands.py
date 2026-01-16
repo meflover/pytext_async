e1e0bfbe49a837f095c9e025f46446e770ae24ab
meflover <guextywq@gmail.com>
2026-01-16 15:05:49 +0300

    Add files via upload

--- handlers/commands.py
+++ handlers/commands.py
@@ -2,10 +2,11 @@ from bot import bot, dp
 from session import session
 from aiogram import F
 from aiogram.types import Message
-
+from handlers.safesend import reply
 @dp.message(F.text == "/start")
 async def start_handler(message: Message):
     user_id = message.from_user.id
     if user_id in session.users:
         del session.users[user_id]
     user = session.short_init(user_id)
+    await reply(message.reply, "Пришлите ваш текст!")
