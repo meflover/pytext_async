55cb90303de59bd434f020cb1466c400454f29b0
meflover <guextywq@gmail.com>
2026-01-16 15:04:45 +0300

    Add files via upload

---
+++ safesend.py
@@ -0,0 +1,17 @@
+from ratelimiter import limiter
+
+async def safe_send(send_func, *args, **kwargs):
+    try:
+        limiter.acquire()
+        result = await send_func(*args, **kwargs)
+        print(f"[SAFE_SEND] {send_func.__name__}: {limiter.score}")
+        return result
+    except Exception as e:
+        print(f"[ERROR] safe_send: {e}\n{args}\n{send_func}")
+        return None
+
+
+async def answer(message, text):
+    await safe_send(message.answer, text)
+async def reply(message, text):
+    await safe_send(message.reply, text)
